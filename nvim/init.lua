89f66940ed839ec002bd9e58403fc84909c79aae
Anthony <189166184+Anthony-Queen@users.noreply.github.com>
2026-01-23 16:41:09 +0100

    Add Catpuccin theme to Nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,22 @@
 vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+
 require("lazy").setup({
+  -- Wakatime plugin
   { "wakatime/vim-wakatime", lazy = false },
+
+  -- Catppuccin colorscheme
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- Also available: latte, frappe, macchiato
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
 })
 
 vim.cmd [[
